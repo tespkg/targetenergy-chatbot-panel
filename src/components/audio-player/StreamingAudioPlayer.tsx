c7560431c24aafd38baaa1cc83cc5de15f5e2dec
ItIsAram-tes-desktop <salehzadeh.jamal@gmail.com>
2023-11-28 15:20:41 +0330

    refactor;Changes playing and processing states to audio-player-state enum

--- src/components/audio-player/StreamingAudioPlayer.tsx
+++ src/components/audio-player/StreamingAudioPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
 import { textToSpeech } from '../../api/chatbot-api'
 import { Button } from '../button/Button'
 import { AudioUtils } from './utils'
@@ -8,7 +8,7 @@ import PlayIcon from 'img/icons/play-icon.svg'
 import PauseIcon from 'img/icons/pause-icon.svg'
 import TextTpSpeechIcon from 'img/icons/text-to-speech-icon.svg'
 import TextTpSpeechConvertIcon from 'img/icons/text-to-speech-in-progress-icon.svg'
-import { STREAMING_AUDIO_PLAYER } from './constants'
+import { STREAMING_AUDIO_PLAYER_EVENT, STREAMING_AUDIO_PLAYER_STATE } from './constants'
 // import TextTpSpeechInProgressIcon from 'img/icons/text-to-speech-in-progress-icon.svg'
 
 const AUDIO_BUFFER_SIZE_THRESHOLD = 128 * 1024
@@ -20,9 +20,11 @@ interface Props {
 
 export const StreamingAudioPlayer = ({ text, id }: Props) => {
   /** States */
-  const [isPlaying, setPlaying] = useState(false)
+  const [audioPlayerState, setAudioAudioPlayerState] = useState<STREAMING_AUDIO_PLAYER_STATE>(
+    STREAMING_AUDIO_PLAYER_STATE.IDLE
+  )
   const playingState = useRef<boolean>(false)
-  const [isConvertingTextToSpeech, setConvertingTextToSpeech] = useState(false)
+  console.log('audioPlayerState:', audioPlayerState)
 
   /** References */
   const audioContextRef = useRef<AudioContext>(null!)
@@ -37,12 +39,12 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
     // Let's play the audio on first audio buffer
     if (queuedAudioBuffers.current.length === 1) {
       startedAt.current = audioContextRef.current.currentTime
-      StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER.PLAY, id)
+      StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER_EVENT.PLAY, id)
     }
   }
   //
   const convertTextToSpeech = (text: string, stream: boolean) => {
-    setConvertingTextToSpeech(true)
+    setAudioAudioPlayerState(STREAMING_AUDIO_PLAYER_STATE.PROCESSING)
     textToSpeech({
       text: text,
       stream: stream,
@@ -83,11 +85,11 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
         }
         read().then(() => {
           // Maybe we can delete all previous buffers and creating a completed buffer
-          setConvertingTextToSpeech(false)
+          setAudioAudioPlayerState(STREAMING_AUDIO_PLAYER_STATE.IDLE)
         })
       })
       .catch(() => {
-        setConvertingTextToSpeech(false)
+        setAudioAudioPlayerState(STREAMING_AUDIO_PLAYER_STATE.IDLE)
       })
   }
   //
@@ -96,11 +98,11 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
   }
   //
   const onResumeClick = () => {
-    StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER.RESUME, id)
+    StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER_EVENT.RESUME, id)
   }
   //
   const onPauseClick = () => {
-    StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER.PAUSE, id)
+    StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER_EVENT.PAUSE, id)
   }
   //
   const createAudioBufferSourceNodeFromAudioBuffer = (audioBuffer: AudioBuffer) => {
@@ -112,11 +114,11 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
         // it is time to play next source
         console.log('There are more queued audio buffers, lets play next')
         currentPlayingBufferIndex.current = currentPlayingBufferIndex.current + 1
-        StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER.PLAY, id)
+        StreamingAudioPlayerEvents.publish(STREAMING_AUDIO_PLAYER_EVENT.PLAY, id)
       } else {
         console.log('All Played. Lets stop source.')
         playingState.current = false
-        setPlaying(false)
+        setAudioAudioPlayerState(STREAMING_AUDIO_PLAYER_STATE.IDLE)
         currentPlayingBufferIndex.current = 0
         audioContextRef.current.suspend().then(() => {
           audioContextRef.current.close().then(() => {
@@ -143,7 +145,7 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
       audioContextRef.current = new (window.AudioContext || window.webkitAudioContext)()
     }
     playingState.current = true
-    setPlaying(true)
+    setAudioAudioPlayerState(STREAMING_AUDIO_PLAYER_STATE.PLAYING)
     const audioBuffer = get(queuedAudioBuffers.current, currentPlayingBufferIndex.current)
     const source = createAudioBufferSourceNodeFromAudioBuffer(audioBuffer)
     currentPlayingAudioSource.current = source
@@ -154,7 +156,7 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
   const pauseEventListener = () => {
     console.log('Pause Event Listener.')
     playingState.current = false
-    setPlaying(false)
+    setAudioAudioPlayerState(STREAMING_AUDIO_PLAYER_STATE.PAUSED)
     pausedAt.current = audioContextRef.current.currentTime
     audioContextRef.current.suspend().then(() => {
       audioContextRef.current.close().then(() => {
@@ -168,7 +170,7 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
   const playEventListener = () => {
     console.log('Plat Event Listener.')
     playingState.current = true
-    setPlaying(true)
+    setAudioAudioPlayerState(STREAMING_AUDIO_PLAYER_STATE.PLAYING)
     const audioBuffer = get(queuedAudioBuffers.current, currentPlayingBufferIndex.current)
     const { source } = playBuffer(audioBuffer)
     if (source) {
@@ -190,27 +192,45 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
   }, [])
   //
   useEffect(() => {
-    StreamingAudioPlayerEvents.subscribe(STREAMING_AUDIO_PLAYER.PLAY, id, playEventListener)
-    StreamingAudioPlayerEvents.subscribe(STREAMING_AUDIO_PLAYER.RESUME, id, resumeEventListener)
-    StreamingAudioPlayerEvents.subscribe(STREAMING_AUDIO_PLAYER.PAUSE, id, pauseEventListener)
+    console.log('Subscribing to Streaming Audio Player Events.')
+    StreamingAudioPlayerEvents.subscribe(STREAMING_AUDIO_PLAYER_EVENT.PLAY, id, playEventListener)
+    StreamingAudioPlayerEvents.subscribe(STREAMING_AUDIO_PLAYER_EVENT.RESUME, id, resumeEventListener)
+    StreamingAudioPlayerEvents.subscribe(STREAMING_AUDIO_PLAYER_EVENT.PAUSE, id, pauseEventListener)
     return () => {
-      StreamingAudioPlayerEvents.unsubscribe(STREAMING_AUDIO_PLAYER.PLAY, id, () => {})
-      StreamingAudioPlayerEvents.unsubscribe(STREAMING_AUDIO_PLAYER.RESUME, id, () => {})
-      StreamingAudioPlayerEvents.unsubscribe(STREAMING_AUDIO_PLAYER.PAUSE, id, () => {})
+      StreamingAudioPlayerEvents.unsubscribe(STREAMING_AUDIO_PLAYER_EVENT.PLAY, id, () => {})
+      StreamingAudioPlayerEvents.unsubscribe(STREAMING_AUDIO_PLAYER_EVENT.RESUME, id, () => {})
+      StreamingAudioPlayerEvents.unsubscribe(STREAMING_AUDIO_PLAYER_EVENT.PAUSE, id, () => {})
     }
-  }, [])
+  }, [id])
+
+  /** Memos */
+  const isConvertTextToSpeechButtonEnable = useMemo(() => {
+    return audioPlayerState === STREAMING_AUDIO_PLAYER_STATE.IDLE && queuedAudioBuffers.current.length === 0
+  }, [audioPlayerState])
+  //
+  const isPauseButtonEnable = useMemo(() => {
+    return audioPlayerState === STREAMING_AUDIO_PLAYER_STATE.PLAYING
+  }, [audioPlayerState])
+  //
+  const isResumeButtonEnable = useMemo(() => {
+    return (
+      (audioPlayerState === STREAMING_AUDIO_PLAYER_STATE.PAUSED ||
+        audioPlayerState === STREAMING_AUDIO_PLAYER_STATE.IDLE) &&
+      queuedAudioBuffers.current.length > 0
+    )
+  }, [audioPlayerState])
+  //
 
   /** Renderer */
   return (
     <div>
-      {isPlaying && (
+      {isPauseButtonEnable && (
         <Button title={'Pause'} onClick={onPauseClick} displayTitle={false} frame={false} imageSource={PauseIcon} />
       )}
-      {!isPlaying && queuedAudioBuffers.current.length > 0 && (
+      {isResumeButtonEnable && (
         <Button title={'Resume'} onClick={onResumeClick} displayTitle={false} frame={false} imageSource={PlayIcon} />
       )}
-
-      {!isPlaying && queuedAudioBuffers.current.length == 0 && !isConvertingTextToSpeech && (
+      {isConvertTextToSpeechButtonEnable && (
         <Button
           title={'Text to Speech'}
           onClick={onConvertTextToAudioClick}
@@ -219,7 +239,7 @@ export const StreamingAudioPlayer = ({ text, id }: Props) => {
           imageSource={TextTpSpeechIcon}
         />
       )}
-      {!isPlaying && queuedAudioBuffers.current.length == 0 && isConvertingTextToSpeech && (
+      {audioPlayerState === STREAMING_AUDIO_PLAYER_STATE.PROCESSING && (
         <Button
           title={'Converting Text to Speech'}
           onClick={() => {}}

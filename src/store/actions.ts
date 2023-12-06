6b39bb576f168801620079cd442d278454501f5d
ItIsAram-tes-desktop <salehzadeh.jamal@gmail.com>
2023-12-06 15:07:02 +0330

    refactor;Moves chat-content to redux store

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,6 +1,7 @@
 import { createAction } from "redux-actions";
 import { sprintf } from "sprintf-js";
 import { LlmTrace } from "../core/orchestration/llm-callbacks";
+import { ChatBotMessage } from "../commons/types/ChatMessagePanelTypes";
 //
 const ACTION_PREFIX = "CHATBOT_PANEL_%s";
 export const AddTrace = createAction(sprintf(ACTION_PREFIX, "ADD_TRACE"), (trace: LlmTrace) => ({ trace }));
@@ -9,3 +10,28 @@ export const SetInoPanelMessageId = createAction(
   sprintf(ACTION_PREFIX, "SET_INFO_PANEL_MESSAGE_ID"),
   (messageId: string) => ({ messageId })
 );
+
+export const AddChatbotMessage = createAction(
+  sprintf(ACTION_PREFIX, "ADD_CHATBOT_MESSAGE"),
+  (messages: ChatBotMessage[]) => ({
+    messages,
+  })
+);
+
+export const DeleteChatbotMessage = createAction(
+  sprintf(ACTION_PREFIX, "DELETE_CHATBOT_MESSAGE"),
+  (messageId: string, parentMessageId: string) => ({
+    messageId,
+    parentMessageId,
+  })
+);
+
+export const UpdateChatbotMessage = createAction(
+  sprintf(ACTION_PREFIX, "UPDATE_CHATBOT_MESSAGE"),
+  (text: string, parentMessageId: string) => ({
+    text,
+    parentMessageId,
+  })
+);
+
+export const ResetChatbotMessage = createAction(sprintf(ACTION_PREFIX, "RESET_CHATBOT_MESSAGE"), () => ({}));
--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -2,6 +2,9 @@ import { handleActions } from "redux-actions";
 import update from "immutability-helper";
 import * as Actions from "./actions";
 import { StoreType } from "./types/general-state-types";
+import { last, uniqueId } from "lodash";
+import { CHATBOT_ROLE, SUPPORTED_MESSAGE_TYPE } from "../commons/enums/Chatbot";
+import { ChatBotMessage } from "../commons/types/ChatMessagePanelTypes";
 
 const initialState: StoreType = {
   traces: [],
@@ -28,6 +31,95 @@ export default handleActions<StoreType, any>(
           $set: messageId,
         },
       });
+    },
+    [Actions.AddChatbotMessage.toString()](
+      state = initialState,
+      { payload }: ReturnType<typeof Actions.AddChatbotMessage>
+    ) {
+      const { messages } = payload;
+      let newChatContent = [...(state.chatContent || [])];
+      newChatContent = [...newChatContent, ...messages];
+      return update(state, {
+        chatContent: {
+          $set: newChatContent,
+        },
+      });
+    },
+    [Actions.DeleteChatbotMessage.toString()](
+      state = initialState,
+      { payload }: ReturnType<typeof Actions.DeleteChatbotMessage>
+    ) {
+      const { messageId, parentMessageId } = payload;
+      const prevMessages = [...(state.chatContent || [])];
+      if (!prevMessages) {
+        return state;
+      }
+
+      const deletedMessage = prevMessages.find((message) => message.id === messageId);
+      if (deletedMessage) {
+        if (deletedMessage.parentMessageId === "parent") {
+          // If the deleted message is parent itself, we need to delete it and all messages which their parent is currently deleted message.
+          const newMessages = prevMessages.filter(
+            (message) => message.id !== messageId && message.parentMessageId !== messageId
+          );
+          return update(state, {
+            chatContent: {
+              $set: newMessages,
+            },
+          });
+        } else {
+          // If the deleted message is child, we need to delete it, the parent and all messages which a mutual parent message.
+          const newMessages = prevMessages.filter(
+            (message) =>
+              message.id !== messageId && message.id !== parentMessageId && message.parentMessageId !== parentMessageId
+          );
+          return update(state, {
+            chatContent: {
+              $set: newMessages,
+            },
+          });
+        }
+      }
+      return state;
+    },
+    [Actions.ResetChatbotMessage.toString()](state = initialState) {
+      return update(state, {
+        chatContent: {
+          $set: undefined,
+        },
+      });
+    },
+    [Actions.UpdateChatbotMessage.toString()](
+      state = initialState,
+      { payload }: ReturnType<typeof Actions.UpdateChatbotMessage>
+    ) {
+      const { text, parentMessageId } = payload;
+      if (state.chatContent !== undefined) {
+        const prevMessages = [...state.chatContent];
+        const lastMessage = last(prevMessages)!;
+        if (lastMessage.role === CHATBOT_ROLE.ASSISTANT) {
+          lastMessage.message = lastMessage.message + text;
+          const newMessages = [...prevMessages.slice(0, prevMessages.length - 1), lastMessage];
+          return update(state, {
+            chatContent: { $set: newMessages },
+          });
+        } else {
+          const newMessages = [
+            ...prevMessages,
+            {
+              id: uniqueId("text_message"),
+              parentMessageId: parentMessageId,
+              role: CHATBOT_ROLE.ASSISTANT,
+              message: text,
+              includeInContextHistory: true,
+              includeInChatPanel: true,
+              type: SUPPORTED_MESSAGE_TYPE.TEXT,
+            } as ChatBotMessage,
+          ];
+          return update(state, { chatContent: { $set: newMessages } });
+        }
+      }
+      return state;
     },
   },
   initialState
